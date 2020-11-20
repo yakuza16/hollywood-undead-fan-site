1691886c6254c11fa35fa692432147ee78f94b69
Marcin Spławiński <marcin.cincin9010@gmail.com>
2020-11-20 14:51:15 +0100

    band info

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,37 +1,50 @@
 <template>
   <Layout>
-    <section class="flex justify-center flex-wrap w-full">
-      <g-image
-        alt="Example image"
-        src="../../static/assets/hollywood-undead-sign.png"
-        class="m-16 px-8 w-full opacity-90 hover:opacity-50 lg:w-2/3"
-      />
-      <g-image
-        alt="Example image"
-        src="../../static/assets/dove-and-grenade.png"
-        class="mx-16 w-full opacity-90 h-auto  hover:opacity-50 lg:w-1/4"
-      />
-    </section>
-    <article class="flex justify-center text-center mx-5">
-      <p class="mb-24 mt-5">
-        Hollywood Undead is an American rap rock band from Los Angeles,
-        California, formed in 2005. They released their debut album, Swan Songs,
-        on September 2, 2008, and their live CD/DVD Desperate Measures, on
-        November 10, 2009. Their second studio album, American Tragedy, was
-        released April 5, 2011. All of the band members use pseudonyms and
-        previously wore their own unique mask, most of which were based on the
-        common hockey goaltender design. The band members currently consist of
-        Charlie Scene, Danny, Funny Man, J-Dog, and Johnny 3 Tears. Their third
-        studio album, titled Notes from the Underground, was released on January
-        8, 2013.[1] Their fourth studio album, Day of the Dead, was released on
-        March 31, 2015.[2] Hollywood Undead's fifth record is titled Five (or
-        V), and was released on October 27, 2017.[3] The first single from the
-        album, called "California Dreaming", was made available July 24,
-        2017.[4] Their sixth studio album, New Empire, Vol. 1, was released on
-        February 14, 2020.[5], and the last [6] New Empire, Vol. 2 was released
-        on december 2020.
-      </p>
-    </article>
+    <div class="flex flex-col justify-center place-items-center mx-4 mt-4">
+      <section class="flex justify-center flex-wrap w-full">
+        <g-image
+          alt="Example image"
+          src="../../static/assets/hollywood-undead-sign.png"
+          class="m-16 px-8 w-full opacity-90 hover:opacity-50 lg:w-2/3"
+        />
+        <g-image
+          alt="Example image"
+          src="../../static/assets/dove-and-grenade.png"
+          class="mx-16 w-full opacity-90 h-auto  hover:opacity-50 lg:w-1/4"
+        />
+      </section>
+      <article class="flex justify-center text-center mx-5 lg:w-3/4 mb-4">
+        <p class="mb-24 mt-8 md:text-lg lg:text-xl">
+          <strong><em>Hollywood Undead</em></strong> is an American rap rock
+          band from Los Angeles, California, formed in 2005. They released their
+          debut album, <strong><em>Swan Songs</em></strong
+          >, on September 2, 2008, and their live CD/DVD
+          <strong><em>Desperate Measures</em> </strong>, on November 10, 2009.
+          Their second studio album,
+          <strong><em>American Tragedy</em></strong> , was released April 5,
+          2011. All of the band members use pseudonyms and previously wore their
+          own unique mask, most of which were based on the common hockey
+          goaltender design. The band members currently consist of
+          <strong
+            ><em
+              >Charlie Scene, Danny, Funny Man, J-Dog, and Johnny 3 Tears</em
+            ></strong
+          >
+          . Their third studio album, titled
+          <strong><em>Notes from the Underground</em></strong> , was released on
+          January 8, 2013. Their fourth studio album,
+          <strong><em> Day of the Dead</em></strong
+          >, was released on March 31, 2015. Hollywood Undead's fifth record is
+          titled <strong><em>Five</em></strong> (or V) , and was released on
+          October 27, 2017. The first single from the album, called "California
+          Dreaming", was made available July 24, 2017. Their sixth studio album,
+          <strong><em>New Empire, Vol. 1</em></strong
+          >, was released on February 14, 2020, and the last
+          <strong><em>New Empire, Vol. 2</em></strong> was released on december
+          4 2020.
+        </p>
+      </article>
+    </div>
   </Layout>
 </template>
 
