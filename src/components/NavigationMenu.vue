a9bff8cc519b6a5d4c747c5972d8b5633bf43aea
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2020-11-30 19:17:40 +0100

    dark mode added

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -2,7 +2,7 @@
   <nav
     class="relative z-10 text-xl bg-gradient-to-r from-teal-300 to-blue-600 py-4 sm:h-screen lg:h-full"
   >
-    <ul class="flex flex-col ml-4 lg:flex-row ">
+    <ul class="flex flex-col ml-4 place-items-center lg:flex-row">
       <li
         v-for="page in routesPages"
         :key="page.name"
@@ -13,12 +13,17 @@
           class="absolute left-0 h-1 w-0 border-1 rounded-3xl bg-gradient-to-r from-teal-400 to-blue-600 ownNav__span"
         ></span>
       </li>
+      <DarkMode class="ml-20"></DarkMode>
     </ul>
   </nav>
 </template>
 
 <script>
+import DarkMode from "vue-dark-mode-switcher";
 export default {
+  components: {
+    DarkMode,
+  },
   data() {
     return {
       routesPages: [
@@ -52,5 +57,10 @@ export default {
 
 .ownNav:hover .ownNav__span {
   width: 80%;
+}
+
+.theme-dark {
+  background: rgba(0, 0, 0, 0.863);
+  color: white;
 }
 </style>
